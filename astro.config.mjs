b9f8b0f0ba57b0cb6f0f67914eb80a333dd05aea
Akshay S Dinesh <akshay@learnlearn.in>
2025-11-18 17:58:33 +0530

    chore: upgrade dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://osmkerala.github.io",
-  integrations: [react(), tailwind(), mdx()]
+  integrations: [react(), mdx()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
